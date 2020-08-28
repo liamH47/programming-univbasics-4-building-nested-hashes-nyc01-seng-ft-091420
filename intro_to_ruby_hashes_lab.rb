66e6a347a2a3c89342ee6ca39b84d96865f21245
friendly-machine-4891 <liamhanafee@gmail.com>
2020-08-28 18:40:53 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -6,10 +6,10 @@ end
 def monopoly_with_second_tier
   monopoly = {
     :railroads =>{
-    pieces: "4"
-    rent_in_dollars: {}
-    names: {}
-    }
+      :pieces => "4",
+      :rent_in_dollars: {
+      :names: {
+    
   }
 end
 
