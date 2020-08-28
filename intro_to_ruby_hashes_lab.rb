e159a03698ae675e5982fc00372f395d3c6d7b08
friendly-machine-4891 <liamhanafee@gmail.com>
2020-08-28 18:48:49 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,9 +34,7 @@ def monopoly_with_third_tier
         },
       :names=>
         {
-          :reading_railroad =>  {
-        "mortgage_value"=>"$100"
-      },
+          :reading_railroad =>  {},
           :pennsylvania_railroad => {},
           :b_and_o_railroad => {},
           :shortline_railroad => {}
@@ -59,7 +57,10 @@ def monopoly_with_fourth_tier
         },
       :names=>
         {
-          :reading_railroad => {},
+          :reading_railroad => 
+          {
+            :mortgage_value
+          },
           :pennsylvania_railroad => {},
           :b_and_o_railroad => {},
           :shortline_railroad => {}
